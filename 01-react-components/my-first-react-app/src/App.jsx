a3a63d83032f2d47e836e754cccb80d1eb63da59
avanich28 <arpawan.v@gmail.com>
2023-09-21 19:06:21 +0700

    complete lesson 01

---
+++ 01-react-components/my-first-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return <div>Hello, React!</div>;
+}
+
+export function Greeting() {
+  return (
+    <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+  );
+}
